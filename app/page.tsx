b9b0fc0d7b7eb2797309db12082de27adca1f673
Rayane Guemmoud <guemmoudrayane03@gmail.com>
2025-10-12 17:19:39 +0200

    Checkpoint from VS Code for coding agent session

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import BlocklyBootstrap from "@/app/_components/BlocklyBootstrap";
+
+export default function Home() {
+  return (
+    <div>
+      <main>
+        <BlocklyBootstrap fullScreen={true} />
+      </main>
+    </div>
+  );
+}
